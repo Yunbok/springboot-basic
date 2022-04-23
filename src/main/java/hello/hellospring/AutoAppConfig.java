c647be32293a729a12385f25776eaba845a83075
Yunbok <qodbsqhr@naver.com>
2022-04-24 01:18:02 +0900

    Spring Component Scan Test

--- src/main/java/hello/hellospring/AutoAppConfig.java
+++ src/main/java/hello/hellospring/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package hello.hellospring;
 
 
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,10 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
+
---
+++ src/test/java/hello/hellospring/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,12 @@
+package hello.hellospring.scan.filter;
+
+import org.springframework.stereotype.Indexed;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+
+}
